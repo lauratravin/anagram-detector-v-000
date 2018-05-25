51dc2990ef3c427406a8bbed96b36a34615fe2c0
Laura Messa Travin <laura.travin@gmail.com>
2018-05-25 17:52:23 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+   attr_accessor :words
+
+   def initialize(w)
+     @words = w
+   end
+
+  def match(collection)
+       collection.each { |chr|
+         if chr.length == @words.length
+             chr.chars == @words.chars
+             return chr
+         else   return []      end   }
+  end
+end
